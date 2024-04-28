bbb2a466610da85ddf70917a4c6433cb7dedf9a8
ravi <mooninthedarknight1999@gmail.com>
2024-04-28 22:57:27 +0530

    implement-product-card

--- src/app/(components)/ProductCard.tsx
+++ src/app/(components)/ProductCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 
 interface ProductInterface {
-  imageUrl: string;
+  imageUrl: string[];
   title: string;
   price: string;
 }
@@ -11,14 +11,29 @@ const ProductCard = ({ product }: { product: ProductInterface }) => {
   const sizes = ["S", "M", "L", "1XL", "2XL", "3XL"];
   const [selectedSize, setSelectedSize] = useState<Boolean>(false);
   const [selectedSizeString, setSelectedSizeString] = useState<String>("");
+  const [currentImageUrl, setCurrentImageUrl] = useState(product.imageUrl[0]);
+  const [currentCard, setCurrentCard] = useState("");
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  console.log(currentImageIndex);
   return (
     <div className="lg:w-96 md:w-72 sm:w-64">
-      <div className="shadow-lg rounded-sm border-red sm:p-2 lg:p-2 md:p-2">
+      <div
+        className="shadow-lg rounded-sm border-red sm:p-2 lg:p-2 md:p-2 cursor-pointer"
+        onClick={() => {
+          setCurrentImageIndex(
+            (currentImageIndex - 1 && currentImageIndex + 1) <=
+              product.imageUrl.length
+              ? currentImageIndex + 1
+              : 0
+          );
+          setCurrentImageUrl(product.imageUrl[currentImageIndex - 1]);
+        }}
+      >
         <Image
           className="w-full"
           width={80}
           height={72}
-          src={product.imageUrl}
+          src={currentImageUrl}
           alt="image"
         />
         <div className="flex justify-between items-center mt-2">
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,8 +6,10 @@ const page = () => {
     <div className="p-5 flex flex-wrap gap-2">
       <ProductCard
         product={{
-          imageUrl:
+          imageUrl: [
             "https://www.nicobar.com/cdn/shop/files/NBI032273_1_400x.jpg?v=1712661312",
+            "https://www.nicobar.com/cdn/shop/files/NBI033408_1_400x.jpg?v=1711309965",
+          ],
           title: "Midnight marvel jacket - Black",
           price: "6750",
         }}
