2f96b11db52ea8bc888237eef8a0880b2ef2a3bd
ravi <mooninthedarknight1999@gmail.com>
2024-05-05 11:55:02 +0530

    Implemented mobile footer

--- src/app/(components)/Footer.tsx
+++ src/app/(components)/Footer.tsx
@@ -2,52 +2,9 @@ import { Instagram, Mail, MapPin } from "lucide-react";
 import { headers } from "next/headers";
 import React from "react";
 import { FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
-
-interface FooterBoxData {
-  iconUrl: string;
-  heading: string;
-  description: string;
-}
-
-const FooterBox: React.FC<{ data: FooterBoxData }> = ({
-  data,
-}: {
-  data: FooterBoxData;
-}) => {
-  return (
-    <div className="w-1/4 bg-slate-400/10 shadow-lg p-5 flex h-32 justify-center items-center">
-      <img src={data.iconUrl} alt="icon" className="h-10 w-10" />
-      <div className="flex flex-col ml-4">
-        <p className="font-bold">{data.heading}</p>
-        <p className="text-sm text-gray-500">{data.description}</p>
-      </div>
-    </div>
-  );
-};
-
-const footerBoxDataArray = [
-  {
-    iconUrl: "https://www.nicobar.com/cdn/shop/files/return.svg?v=1676354450",
-    heading: "Easy returns",
-    description: "Return within 15 days order delivery",
-  },
-  {
-    iconUrl:
-      "https://www.nicobar.com/cdn/shop/files/noun-car-2164405_79b2948c-e083-445b-959b-b2a39a68967f.svg?v=1697622804",
-    heading: "We ship worldwide",
-    description: "visit global.nicobar.com",
-  },
-  {
-    iconUrl: "https://www.nicobar.com/cdn/shop/files/cash.svg?v=1676354450",
-    heading: "Cash on delivery",
-    description: "COD available",
-  },
-  {
-    iconUrl: "https://www.nicobar.com/cdn/shop/files/order.svg?v=1676354450",
-    heading: "Free shipping",
-    description: "Free shipping on ordersabove ₹ 1,000",
-  },
-];
+import { FooterBoxData } from "../(interfaces)/footerboxdata";
+import FooterBox from "../(components)/FooterBox";
+import { footerBoxDataArray } from "../(utils)/constants";
 
 const Footer = () => {
   const date: number = new Date().getFullYear();
@@ -55,7 +12,7 @@ const Footer = () => {
     <div className="font-euclid">
       <div className="w-full bg-slate-400/10 shadow-lg border-gray-300 min-h-56 flex flex-col p-4">
         <div className="flex flex-col items-center justify-center">
-          <p className="font-butler font-extrabold text-center text-2xl text-gray-700">
+          <p className="font-serif font-extrabold text-center text-2xl text-gray-700">
             Get on the list
           </p>
           <p className="text-[14px] text-justify mt-2 w-1/3">
--- src/app/(components)/ResponsiveFooter.tsx
+++ src/app/(components)/ResponsiveFooter.tsx
@@ -2,12 +2,13 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
 import Footer from "./Footer";
+import MobileFooter from "./MobileFooter";
 
 const ResponsiveFooter = () => {
   const isDesktopOrLaptop = useMediaQuery({
     query: "(min-width: 1000px)",
   });
-  return isDesktopOrLaptop ? <Footer /> : null;
+  return isDesktopOrLaptop ? <Footer /> : <MobileFooter />;
 };
 
 export default ResponsiveFooter;
